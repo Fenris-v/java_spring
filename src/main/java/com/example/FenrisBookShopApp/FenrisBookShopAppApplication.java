d28f12e98f80648509190a3ed5347df7ad26b012
Anatoliy Vasilenko <anatolijv236@gmail.com>
2022-09-25 16:34:12 +0300

    Init message

---
+++ src/main/java/com/example/FenrisBookShopApp/FenrisBookShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FenrisBookShopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenrisBookShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenrisBookShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FenrisBookShopApp/FenrisBookShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FenrisBookShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenrisBookShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
