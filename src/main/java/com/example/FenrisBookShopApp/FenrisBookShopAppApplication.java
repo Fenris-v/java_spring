05fa15bb95e88394589fc881472cb7fea1115f4b
Anatoliy Vasilenko <anatolijv236@gmail.com>
2022-10-22 21:27:01 +0300

    homework 5

--- src/main/java/com/example/FenrisBookShopApp/FenrisBookShopAppApplication.java
+++ src/main/java/com/example/FenrisBookShopApp/FenrisBookShopAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FenrisBookShopAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FenrisBookShopAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FenrisBookShopAppApplication.class, args);
+    }
 }
--- src/main/java/com/example/FenrisBookShopApp/data/TestEntity.java
+++ src/main/java/com/example/FenrisBookShopApp/data/TestEntity.java
@@ -2,22 +2,18 @@ package com.example.FenrisBookShopApp.data;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
-@Getter
 @Setter
+@Getter
+@ToString
 @Entity
-@Table(name = "books")
-public class Book {
+@Table(name = "test_entities")
+public class TestEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Transient
-    private String author;
-
-    private String title;
-    private String priceOld;
-    private String price;
+    private Long id;
+    private String data;
 }
