357a1c3e587a7e18899cb7f1d167bdb3c05f4fbd
Anatoliy Vasilenko <anatolijv236@gmail.com>
2022-10-26 17:33:26 +0300

    homework 5

--- src/main/java/com/example/FenrisBookShopApp/data/BookService.java
+++ src/main/java/com/example/FenrisBookShopApp/data/BookService.java
@@ -1,46 +1,20 @@
 package com.example.FenrisBookShopApp.data;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookService {
-    private final JdbcTemplate jbdcTemlate;
+    private final BookRepository bookRepository;
 
     @Autowired
-    public BookService(JdbcTemplate jbdcTemlate) {
-        this.jbdcTemlate = jbdcTemlate;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     public List<Book> getBooksData() {
-        String sql = "SELECT books.*, first_name, last_name FROM books LEFT JOIN authors on authors.id = author_id";
-        List<Book> books = jbdcTemlate.query(sql, (ResultSet rs, int rowNum) -> makeBook(rs));
-        return new ArrayList<>(books);
-    }
-
-    @NotNull
-    private Book makeBook(@NotNull ResultSet rs) throws SQLException {
-        Book book = new Book();
-        book.setId(rs.getInt("id"));
-        book.setAuthor(getAuthorByAuthorId(rs.getInt("author_id")));
-        book.setTitle(rs.getString("title"));
-        book.setPriceOld(rs.getString("price_old"));
-        book.setPrice(rs.getString("price"));
-        return book;
-    }
-
-    private String getAuthorByAuthorId(int author_id) {
-        String sql = "SELECT * FROM authors WHERE authors.id=" + author_id;
-        List<String> authors = jbdcTemlate
-                .query(sql, (ResultSet rs, int rowNum) -> rs.getString("first_name") + " " + rs.getString("last_name"));
-
-        return authors.get(0);
+        return bookRepository.findAll();
     }
 }
