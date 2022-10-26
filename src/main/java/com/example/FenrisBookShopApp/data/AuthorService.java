5431de402e05889fa6c830bb56653fba7bdcbc10
Anatoliy Vasilenko <anatolijv236@gmail.com>
2022-10-26 21:17:36 +0300

    homework 5

--- src/main/java/com/example/FenrisBookShopApp/data/AuthorService.java
+++ src/main/java/com/example/FenrisBookShopApp/data/AuthorService.java
@@ -2,39 +2,24 @@ package com.example.FenrisBookShopApp.data;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 @Service
 public class AuthorService {
-    private final JdbcTemplate jbdcTemlate;
+    private final AuthorRepository authorRepository;
     private final TreeMap<Character, TreeSet<Author>> authorsMap = new TreeMap<>();
 
     @Autowired
-    public AuthorService(JdbcTemplate jbdcTemlate) {
-        this.jbdcTemlate = jbdcTemlate;
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     public TreeMap<Character, TreeSet<Author>> getAuthorsMap() {
-        String sql = "SELECT * FROM authors";
-        List<Author> authors = jbdcTemlate.query(sql, (ResultSet rs, int rowNum) -> makeAuthors(rs));
-
-        return groupAuthors(authors);
-    }
-
-    @NotNull
-    private Author makeAuthors(@NotNull ResultSet rs) throws SQLException {
-        Author author = new Author();
-        author.setId(rs.getInt("id"));
-        author.setFirstName(rs.getString("first_name"));
-        author.setLastName(rs.getString("last_name"));
-        return author;
+        return groupAuthors(authorRepository.findAll());
     }
 
     @NotNull
@@ -44,7 +29,7 @@ public class AuthorService {
     }
 
     private void createGroupByFirstLetter(@NotNull Author author) {
-        char firstLetter = Character.toUpperCase(author.getFirstName().charAt(0));
+        char firstLetter = Character.toUpperCase(author.getName().charAt(0));
         if (!authorsMap.containsKey(firstLetter)) {
             authorsMap.put(firstLetter, new TreeSet<>());
         }
